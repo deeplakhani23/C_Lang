2e639ca1c90af53f03e85dd94173027deffa7781
Deep <deeplakhani9@gmail.com>
2023-08-09 08:46:55 +0530

    a

--- c++/linear_searsh.c
+++ c++/linear_searsh.c
@@ -1,22 +1,20 @@
-#include <stdio.h>
-  
-int search(int arr[], int N, int x)
+#include<stdio.h>
+#include<stdlib.h>
+#define n 10
+int search(int a[], int N, int x)
 {
     for (int i = 0; i < N; i++)
-        if (arr[i] == x)
+        if (a[i] == x)
             return i;
     return -1;
 }
-  
-// Driver code
 int main(void)
 {
-    int arr[] = { 2, 3, 4, 10, 40 };
+    int a[] = { 2, 3, 4, 10, 40 };
     int x = 10;
-    int N = sizeof(arr) / sizeof(arr[0]);
+    int N = sizeof(a) / sizeof(a[0]);
   
-    // Function call
-    int result = search(arr, N, x);
+    int result = search(a, N, x);
     (result == -1)
         ? printf("Element is not present in array")
         : printf("Element is present at index %d", result);
